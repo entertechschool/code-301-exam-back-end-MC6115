c745557d380c530061ee5f1a43065bbcfc43166c
Matias Carrillo Sanchez <Matiascarrillosanchez17@gmail.com>
2024-10-11 18:45:44 -0500

    passes all tests, mock database use correct

--- src/server.js
+++ src/server.js
@@ -8,17 +8,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/iteems', Data.getAllItems);
+app.get('/items', Data.getAllItems);
 app.get('/items/:id', Data.getOneItem);
 app.post('/items', Data.addAnItem);
+app.delete('/items/:id', Data.deleteOneItem)
 
-app.use('*', (req,res) => {
+app.use('*', (req, res) => {
   res.status(404).send('These are not the droids you are looking for.');
 });
 
-app.use( (error,req,res,next) => {
+app.use((error, req, res, next) => {
   res.status(500).send(`My Bad ... ${error.message}`);
 });
 
@@ -27,8 +28,8 @@ app.use( (error,req,res,next) => {
 // There are no errors in the code below. Do not make any changes to these lines.
 module.exports = {
   server: app,
-  start: (port) => {
-    console.log('yo');
-    app.listen(port, console.log(`Server is up and running on port: ${port}`));
-  },
+  // start: (port) => {
+  //   console.log('yo');
+  //   app.listen(port, console.log(`Server is up and running on port: ${port}`));
+  // },
 };
